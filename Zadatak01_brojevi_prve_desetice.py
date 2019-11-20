872fb9c1f31fcb79abc8d51db695ae5ba1bd21c4
CumulusBGD <57710907+CumulusBGD@users.noreply.github.com>
2019-11-20 20:26:17 +0100

    Add files via upload

---
+++ Zadatak01_brojevi_prve_desetice.py
@@ -0,0 +1,8 @@
+#Napisi definiciju koja ce da prikaze koji su brojevi prve desetice
+
+lista = []
+def prvaDesetica ():
+    for x in range (1,11):
+        lista.append (x)
+    return lista
+print ("Brojevi prve desetice su: ",prvaDesetica())
