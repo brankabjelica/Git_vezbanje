c47e2dc9e9990bc2c79f6ef7884ad73befb9390c
mateja <mtrikic2@gmail.com>
2019-11-20 20:19:26 +0100

    matejazadatak

--- PetaGrupa/Zadatak01_brojevi_prve_desetice.py
+++ PetaGrupa/Zadatak01_brojevi_prve_desetice.py
@@ -1,8 +1,8 @@
 #Napisi definiciju koja ce da prikaze koji su brojevi prve desetice
 
-lista=[]
+lista1=[]
     for broj in range(1, 11):
         print(broj)
         lista.append(broj)
 
-print("lista brojeva je:",lista)
+print("lista brojeva je:",lista1)
